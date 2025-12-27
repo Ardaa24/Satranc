f2c095c8d2e54de1d167c27557205f870ae4dab4
Ardacan Süren <ardacansuren@gmail.com>
2025-12-27 12:16:08 +0300

    taş modeli eklendi.

--- Model/ChessPiece.cs
+++ Model/ChessPiece.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Satranc.Model.Enums;
 
 namespace Satranc.Model
 {
     internal class ChessPiece
     {
+        public PieceType Type { get; }
+        public PieceColor Color { get; }
+
+        public ChessPiece(PieceType type, PieceColor color)
+        {
+            Type = type;
+            Color = color;
+        }
     }
+
 }
